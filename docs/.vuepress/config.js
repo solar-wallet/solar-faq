b5741c4b023988a910b9006ae9d1e03d849fa3ff
Erasmus <erasmus@degeist.com>
2019-02-08 16:53:24 +0100

    Add basic styling

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,9 @@
 var getChildren = require('./sidebar');
 
 module.exports = {
-  title: 'Netlify CMS + VuePress',
-  description: 'Netlify + VuePress',
+  title: 'Solar Wallet Guide',
+  description: 'Guide for Solar wallet, the easiest account management for Stellar',
+  logo: '/images/solar-wallet-logo.svg',
   themeConfig: {
     sidebar: getChildren('./docs/guide/') 
   }
