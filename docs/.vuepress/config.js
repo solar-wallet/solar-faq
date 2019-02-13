156b5684f5c408ff9cd7c2c29f59885fc4cc24f4
Erasmus <erasmus@degeist.com>
2019-02-13 10:46:59 +0100

    Quickfix sidebar ordering

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ var getChildren = require('./sidebar');
 module.exports = {
   title: 'Solar Wallet Guide',
   description: 'Guide for Solar wallet, the easiest account management for Stellar',
-  logo: '/images/solar-wallet-logo.svg',
+  logo: './images/solar-wallet-logo.svg',
   themeConfig: {
     sidebar: getChildren('./docs/guide/') 
   }
