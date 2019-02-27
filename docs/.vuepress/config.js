1a57a0609826bbeacec155675c48c89f4227071e
Erasmus <erasmus@degeist.com>
2019-02-27 17:46:08 +0100

    Add logo in navbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,8 @@ var getChildren = require('./sidebar');
 module.exports = {
   title: 'Solar Wallet Guide',
   description: 'Guide for Solar wallet, the easiest account management for Stellar',
-  logo: './images/solar-wallet-logo.svg',
   themeConfig: {
+    logo: '/images/solar-wallet-logo.svg',
     sidebar: getChildren('./docs/guide/'),
     sidebarDepth: 2
   }
